1c41ff97cb52c3e9a14004bf63fb8f11794d7423
Monir <mrmonir0558@gmail.com>
2025-03-06 20:58:48 +0600

    added dark mode

--- app/layout.js
+++ app/layout.js
@@ -18,8 +18,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
-            <body className={`${outfitFont.className} ${ovoFont.className} antialiased`}>{children}</body>
+        <html lang="en" className='scroll-smooth dark'>
+            <body className={`${outfitFont.className} ${ovoFont.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}>{children}</body>
         </html>
     );
 }
